6209b9330f57a8d5ce851a1b41b4606e372e6324
SamM71 <sammocock@gmail.com>
2023-07-21 15:40:42 +0100

    add colours using displayWinner()

--- index.js
+++ index.js
@@ -15,15 +15,21 @@ const playGame = () => {
     return determineWinner(userChoice, compChoice);
 }
 
+const displayWinner = (result) => {
+    if (result === "draw") {
+        console.log(colours.yellow("The game is a draw!"));
+    } else if (result === "user") {
+        console.log(colours.green("User is the winner!"));
+    } else {
+        console.log(colours.red("Computer is the winner!"));
+    }
+}
+
 while (playAgain) {
 
     const result = playGame();
     
-    if (result === "draw") {
-        console.log("The game is a draw!");
-    } else {
-        console.log(`${result} is the winner!`);
-    }
+    displayWinner(result);
 
     const answer = prompt("Enter 'y' to play again: ");
     if (answer !== "y") {
