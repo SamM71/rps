53a3449ec5e187d3ceb1bc12c507f5984776179c
SamM71 <sammocock@gmail.com>
2023-07-21 15:20:41 +0100

    add loop

--- index.js
+++ index.js
@@ -2,17 +2,28 @@ const colours = require("ansi-colors");
 const prompt = require("prompt-sync")({sigint: true});
 const { determineWinner, compChoose } = require("./rps.js");
 
-const userChoice = prompt("Please choose rock, paper, or scissors: ").toLowerCase();
 
-const compChoice = compChoose();
 
-console.log(`User chose ${userChoice}!`);
-console.log(`Computer chose ${compChoice}!`)
+let playAgain = true;
 
-const result = determineWinner(userChoice, compChoice);
+while (playAgain) {
+    const userChoice = prompt("Please choose rock, paper, or scissors: ").toLowerCase();
 
-if (result === "draw") {
-    console.log("The game is a draw!");
-} else {
-    console.log(`${result} is the winner!`);
+    const compChoice = compChoose();
+    
+    console.log(`User chose ${userChoice}!`);
+    console.log(`Computer chose ${compChoice}!`)
+    
+    const result = determineWinner(userChoice, compChoice);
+    
+    if (result === "draw") {
+        console.log("The game is a draw!");
+    } else {
+        console.log(`${result} is the winner!`);
+    }
+
+    const answer = prompt("Enter 'y' to play again: ");
+    if (answer !== "y") {
+        playAgain = false;
+    }
 }
