a74ed361275b64bf4a89a065c971bfc4abfbd5f0
SamM71 <sammocock@gmail.com>
2023-07-21 15:30:42 +0100

    refactor index with playGame

--- index.js
+++ index.js
@@ -2,19 +2,22 @@ const colours = require("ansi-colors");
 const prompt = require("prompt-sync")({sigint: true});
 const { determineWinner, compChoose } = require("./rps.js");
 
-
-
 let playAgain = true;
 
-while (playAgain) {
+const playGame = () => {
     const userChoice = prompt("Please choose rock, paper, or scissors: ").toLowerCase();
 
     const compChoice = compChoose();
     
     console.log(`User chose ${userChoice}!`);
     console.log(`Computer chose ${compChoice}!`)
-    
-    const result = determineWinner(userChoice, compChoice);
+
+    return determineWinner(userChoice, compChoice);
+}
+
+while (playAgain) {
+
+    const result = playGame();
     
     if (result === "draw") {
         console.log("The game is a draw!");
